d8653c7b5af0f72c7df9a26a83ef3822456a5572
lukaszowaty <lzwolan@wp.pl>
2016-10-02 18:17:48 +0200

    Bibloteka - gotowa

--- JavaSbS42/src/app/LibraryApp.java
+++ JavaSbS42/src/app/LibraryApp.java
@@ -3,7 +3,7 @@ package app;
 public class LibraryApp {
 
 	public static void main(String[] args) {
-		final String appName = "Bibloteka v0.9";
+		final String appName = "Bibloteka v1.0";
 		System.out.println(appName);
 		LibraryControl libControl = new LibraryControl();
 		libControl.controlLoop();
