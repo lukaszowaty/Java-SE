42ebb060b9fa5b752a92b37969989cbea84e2302
Łukasz <lzwolan@wp.pl>
2016-10-04 15:18:51 +0200

    Mp3 player in JavaFX

---
+++ Mp3Player/src/pl/javastart/mp3player/controller/ControlPaneController.java
@@ -0,0 +1,53 @@
+package pl.javastart.mp3player.controller;
+ 
+import java.net.URL;
+import java.util.ResourceBundle;
+ 
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Slider;
+import javafx.scene.control.ToggleButton;
+ 
+public class ControlPaneController implements Initializable{
+ 
+    @FXML
+    private Button nextButton;
+ 
+    @FXML
+    private Slider volumeSlider;
+ 
+    @FXML
+    private Slider songSlider;
+ 
+    @FXML
+    private Button prevButton;
+ 
+    @FXML
+    private ToggleButton playButton;
+     
+    public Button getPrevButton() {
+        return prevButton;
+    }
+     
+    public Button getNextButton() {
+        return nextButton;
+    }
+     
+    public ToggleButton getPlayButton() {
+        return playButton;
+    }
+     
+    public Slider getVolumeSlider() {
+        return volumeSlider;
+    }
+     
+    public Slider getSongSlider() {
+        return songSlider;
+    }
+ 
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+    }
+ 
+}
---
+++ Mp3Player/src/pl/javastart/mp3player/main/Main.java
@@ -0,0 +1,31 @@
+package pl.javastart.mp3player.main;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+	
+	@Override
+	public void start(Stage primaryStage) {
+		final String appName = "MP3Player v0.9";
+		
+		try {
+            Parent parent = (Parent) FXMLLoader.load(getClass().getResource(
+                    "/pl/javastart/mp3player/view/MainPane.fxml"));
+            Scene scene = new Scene(parent);
+            primaryStage.setTitle(appName);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+	}
+	
+	public static void main(String[] args) {
+	    launch(args);
+	}
+
+}
