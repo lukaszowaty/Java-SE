4a1a15d27a93cd3d61f9a1da37d8c3d9c20d660f
Łukasz Zwolan <lzwolan@wp.pl>
2016-11-14 12:34:33 +0100

    Update Library.java

--- JavaSbS42/src/data/Library.java
+++ JavaSbS42/src/data/Library.java
@@ -7,9 +7,6 @@ import java.util.Map;
 
 public class Library implements Serializable{
     
-    /**
-	 * 
-	 */
 	private static final long serialVersionUID = 2995794334600947814L;
     private Map<String, Publication> publications;
     private Map<String, LibraryUser> users;
